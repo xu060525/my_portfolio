75cdaf379be0ea4e47ca2001a46a1d75603ee050
unknown <2377392781@qq.com>
2026-01-16 16:38:32 +0800

    Security: 使用 decouple 隐藏 SECRET_KEY

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m05gt1y#d)dh8wl!d#6v+obt6cw=2f6q5z-rz92-@dnyn7=y%y'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+# '*' 表示允许任何域名访问，或者以后改成你具体的域名
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,8 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+# 运行 collectstatic 的时候发所有分散的 CSS/JS 收集到哪里
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
